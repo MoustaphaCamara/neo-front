17379f4b7fcd52b63e93fad5e77dc8eed76efb87
Lefido <lefido@hotmail.com>
2023-01-05 11:57:11 +0100

    11em  commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import TestPage from './components/TestPage.vue'
+
+function achter(val) {
+  
+  return val
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <HelloWorld msg="huyguytgutg + Vue" />
+  <TestPage/>
+  {{ achter("Coucou fred") }}
 </template>
+
+
 
 <style scoped>
 .logo {

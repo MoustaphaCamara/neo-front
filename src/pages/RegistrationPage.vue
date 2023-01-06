ab3dc0bc98f5a3e31649d1d8aefdf65bfb994491
Lefido <lefido@hotmail.com>
2023-01-06 11:18:38 +0100

    Ric-Nav-7

--- src/pages/RegistrationPage.vue
+++ src/pages/RegistrationPage.vue
@@ -1,5 +1,50 @@
 <template>
-  <h1>Inscription</h1>
+
+<div class="container-registration">
+
+  <div class="inscription-logo">
+    <h1>Inscription</h1>
+
+    <div class="logo">
+
+      <i class="fa-regular fa-user"></i>
+      <!-- <img src="/" alt="logo"> -->
+      <!-- <img v-if="src" src="/" alt="logo"> -->
+    </div>
+
+  </div>
+ 
+
+  <form action="/" method="post">
+   
+        <label for="name">Nom</label>
+        <input type="text" id="name" name="user_name">
+
+        <label for="ceo-name">Nom CEO</label>
+        <input type="text" id="ceo-name" name="ceo_name">
+   
+   
+        <label for="mail">E-mail</label>
+        <input type="email" id="mail" name="user_mail">
+
+        <label for="phone">Téléphone</label>
+        <input id="phone" type="tel" pattern="[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}" name="phone">
+
+        <label for="website">Site Web</label>
+        <input id="website" type="text" name="website">
+
+        <label for="psw1">Mot de passe :</label>
+        <input id="psw1" type="password" name="password">
+   
+    
+        <label for="psw2">Confirmation du mot passe :</label>
+        <input id="psw2" type="password" name="password2">
+
+        <button type="submit">Confirmer</button>
+
+</form>
+
+</div>
   <!-- on peut essayer de ne faire qu'une page inscription pour les 2 (candidat & agence)
   si ça peut marcher comme ça :
   un select entre "candidat" - "choix", si on sélectionne candidat on affiche que tel et tel input, si agence on affiche les autres input
@@ -13,4 +58,47 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+
+.inscription-logo {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid black;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  border: 1px solid black;
+}
+
+.fa-user {
+ font-size: 32px;
+}
+
+.container-registration {
+  width: 400px;
+  padding: 10px;
+  border: 1px solid black;
+  margin: auto;
+ 
+}
+
+form {
+  width: 95%;
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  gap: 5px;
+ 
+}
+
+
+
+</style>

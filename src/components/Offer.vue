881024a45429d2925a304252d05464806de93fcd
Lefido <lefido@hotmail.com>
2023-01-06 14:00:09 +0100

    ric-Candidate-home9

--- src/components/Offer.vue
+++ src/components/Offer.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h1>Offre : {{ title }}</h1>
+  <div class="offer">
+    <h1>{{ title }}</h1>
     <p>{{ content }}</p>
     <p>Type de contrat : {{ contrat_type }}</p>
     <p>Salaire annuel : {{ salary }}</p>
@@ -12,16 +12,33 @@ export default {
   name: "Offer",
   data() {
     return {
-      title: "Développeur•se JS/React",
+      title: "Développeur•se JS/Vue",
       content:
         "Présentation de l'entreprise.Semji est un éditeur qui commercialise une 👉plateforme SaaS👈 de création de contenus digitaux, propulsée par de lIA 🚀, accompagnée de services professionnels SEO. Semji se différencie de ses concurrents par une expérience client unique apportée par sa plateforme et les services professionnels associés.Nos équipes sont animées par la recherche de la performance, de l’innovation et de la satisfaction Client. Si tu aimes partager, échanger et innover pour grandir ensemble, alors tu seras comme un poisson dans l'eau chez Semji !",
       contrat_type: "CDI",
       salary: "37k",
 
-        
     };
   },
 };
 </script>
 
-<style></style>
+<style>
+
+.offer {
+  background-color: var(--bg-sable);
+  width: 100%;
+  padding: 10px;
+  border-radius: 10px;
+}
+
+.offer h1 {
+  color: var(--text-hover-orange);
+  font-size: 30px;
+}
+
+.offer p {
+  margin: 5px;
+}
+
+</style>

9964f211b96b33a69af2acb384800688a7af5ab1
Nawel Grine <108469619+Nawshedev@users.noreply.github.com>
2023-01-11 09:19:13 +0100

    ajout de la barre de recherche et des offres d'emplois dans la home page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,28 @@
 <template>
   <div class="home-container">
     <Banniere :randomUser="randomUser" />
+    
+  <div class="search-section"> 
+    <h2>Tu recherches une offre en particulier ?</h2>
+    <form action="" class="search">
+      <input type="text" name="what" placeholder="Métier, mots-clés ou agence" id="">
+      <input type="text" name="where" placeholder="Nom de la planète" id="">
+      <input type="submit" value="Rechercher">
+    </form>
+  </div>
+
+  <h2>Les dernières offres mises en ligne</h2>
+
+  <section class="liste-offres">
+    <OfferJob />
+    <OfferJob />
+    <OfferJob />
+    <OfferJob />
+    <OfferJob />
+    <OfferJob />
+  </section>
+   
+ 
   </div>
 </template>
 
@@ -8,11 +30,40 @@
 // https://randomuser.me/api
 import Banniere from "../components/Banniere.vue";
 export default {
-  components: { Banniere },
+  components: { Banniere, OfferJob },
   props: {
     randomUser: Array | Object,
   },
 };
+
+import OfferJob from "../components/OfferJob.vue";
 </script>
 
-<style></style>
+<style>
+.search-section, .search-section h2{
+  background-color: rgb(255, 255, 255);
+  color: black;
+  text-align: center;
+  padding: 32px;
+}
+.search {
+  margin-top: 16px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+
+}
+
+h2{
+  color: white;
+  text-align: center;
+}
+
+.liste-offres{
+  display: flex; 
+  flex-wrap: wrap;
+  gap:8px;
+  justify-content: space-evenly;
+}
+
+</style>
